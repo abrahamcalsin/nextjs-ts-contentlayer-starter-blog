a965161e8ae4c93bfc23cb883b55dd8978d98af0
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2023-01-23 10:47:24 -0500

    feat: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Nextjs + TS + ContentLayer</title>
+      </Head>
+      <h1>Nextjs + TS + ContentLayer - Starter Blog</h1>
+    </>
+  );
+}
