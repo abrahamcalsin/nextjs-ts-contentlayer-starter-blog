11640cacb8aea2616c44acb793aeb761244f7aec
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2023-01-30 17:41:35 -0500

    chore: add new property to the post preview component

--- src/components/post-preview/post-preview.tsx
+++ src/components/post-preview/post-preview.tsx
@@ -12,11 +12,14 @@ export default function PostPreview(props: PostPreviewProps) {
       {posts.map(post => {
         return (
           <Link href={`/blog/${post.slug}`} key={post._id}>
-            <h3>{post.title}</h3>
-            <p>
+            <h3 className="post-preview-title">{post.title}</h3>
+            <p className="post-preview-date">
               <DateFormat date={post.date} dateFormat="MMMM D, YYYY" />
             </p>
-            <p>{post.description}</p>
+            <div className="post-preview-cover-image">
+              <img src={post.coverImage} alt={post.slug} />
+            </div>
+            <p className="post-preview-description">{post.description}</p>
           </Link>
         )
       })}
